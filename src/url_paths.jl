9dd48b1e0e20dfcee67244d86fa88fc165f5fd95
RodrigoLRR <75823221+RodrigoLRR@users.noreply.github.com>
2021-02-03 00:26:04 -0600

    Add files via upload

---
+++ src/url_paths.jl
@@ -0,0 +1,6 @@
+url="http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip";
+turl="https://www.coneval.org.mx/Medicion/Documents/Pobreza_municipal/Concentrado_indicadores_de_pobreza.zip";
+murl="http://www.conapo.gob.mx/work/models/CONAPO/intensidad_migratoria/base_completa/IIM2010_BASEMUN.xls";
+path=pwd()*"\\Docs";
+pathCov=pwd()*"\\Docs\\Cov_data";
+pathINEGI=pwd()*"\\Docs\\Datos INEGI\\Datos ";
