2214d6f213ebb05935b01d29682c1ff406ed9f06
OscarJVP <47265985+OscarJVP@users.noreply.github.com>
2021-02-03 04:42:35 -0600

    Add files via upload

--- src/Down_datINEGI.jl
+++ src/Down_datINEGI.jl
@@ -1,17 +1,26 @@
 using CSV,DataFrames
 export COVID_data_tool
-COVID_data_tool.ind_tabla
+#COVID_data_tool.diccionario_indicadores
+#=arreglo_dif=[]
 ind=[]
-for k in keys(COVID_data_tool.ind_est)
+for k in keys(COVID_data_tool.diccionario_estados)
     push!(ind,k)
 end
 ind
+=#
+#=
 for k in ind
     @show k
     COVID_data_tool.datos_indicador("Porcentaje de hombres",k)
 end
+=#
+
+ind = ["Coahuila de Zaragoza","Guerrero","Hidalgo","Nayarit","Querétaro","Quintana Roo"]
 
 for k in ind
     @show k
     COVID_data_tool.datos_indicador("Porcentaje de mujeres",k)
+    #push!(arreglo_dif, k)
 end
+
+#ind=setdiff(ind,arreglo_dif)
