9bd6c13ff224a92e33b784f320f1f9ccb06a85e4
Oscar Vargas <lost9924@gmail.com>
2021-02-03 23:16:11 -0600

    .

--- src/url_paths.jl
+++ src/url_paths.jl
@@ -1,6 +1,6 @@
 url="http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos_covid19.zip";
 turl="https://www.coneval.org.mx/Medicion/Documents/Pobreza_municipal/Concentrado_indicadores_de_pobreza.zip";
 murl="http://www.conapo.gob.mx/work/models/CONAPO/intensidad_migratoria/base_completa/IIM2010_BASEMUN.xls";
-path="Docs";
-pathCov="Docs\\Cov_data";
-pathINEGI="Docs\\Datos INEGI\\Datos ";
+path=chop(pathof(COVID_data_tool), tail=22)*"Docs";
+pathCov=chop(pathof(COVID_data_tool), tail=22)*"Docs\\Cov_data";
+pathINEGI=chop(pathof(COVID_data_tool), tail=22)*"Docs\\Datos INEGI\\Datos ";
