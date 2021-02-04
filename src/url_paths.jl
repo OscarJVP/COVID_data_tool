23608f4a523d5908aeb6a2f4fdb7bc5f89fbd6a5
Oscar Vargas <lost9924@gmail.com>
2021-02-04 15:03:55 -0600

    .

--- src/url_paths.jl
+++ src/url_paths.jl
@@ -2,12 +2,12 @@ url="http://datosabiertos.salud.gob.mx/gobmx/salud/datos_abiertos/datos_abiertos
 turl="https://www.coneval.org.mx/Medicion/Documents/Pobreza_municipal/Concentrado_indicadores_de_pobreza.zip";
 murl="http://www.conapo.gob.mx/work/models/CONAPO/intensidad_migratoria/base_completa/IIM2010_BASEMUN.xls";
 
-if Sys.iswindows
+if Sys.iswindows()
     path=chop(@__DIR__, tail=3)*"Docs";
     pathCov=chop(@__DIR__, tail=3)*"Docs\\Cov_data";
     pathINEGI=chop(@__DIR__, tail=3)*"Docs\\Datos INEGI\\Datos ";
     pathCSV="C:\\archivos_CSV_COVID_data_tool"
-elseif Sys.islinux
+elseif Sys.islinux()
     path=chop(@__DIR__, tail=3)*"Docs";
     pathCov=chop(@__DIR__, tail=3)*"Docs/Cov_data";
     pathINEGI=chop(@__DIR__, tail=3)*"Docs/Datos INEGI/Datos ";
